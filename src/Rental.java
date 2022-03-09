7e2d5e6660e660162d5817720c571acc4a3fd628
Mardin <mardineliassi57@gmail.com>
2022-03-09 14:10:28 +0100

    Rental class added.

---
+++ src/Rental.java
@@ -0,0 +1,57 @@
+public class Rental {
+
+
+    private String fromDateAndTime;
+    private String toDateAndTime;
+    private int maxKm;
+    private Car car;
+    private Customer customer;
+
+    public Rental(String fromDateAndTime, String toDateAndTime, int maxKm, Car car, Customer customer) {
+        this.fromDateAndTime = fromDateAndTime;
+        this.toDateAndTime = toDateAndTime;
+        this.maxKm = maxKm;
+        this.car = car;
+        this.customer = customer;
+    }
+
+    public String getFromDateAndTime() {
+        return fromDateAndTime;
+    }
+
+    public void setFromDateAndTime(String fromDateAndTime) {
+        this.fromDateAndTime = fromDateAndTime;
+    }
+
+    public String getToDateAndTime() {
+        return toDateAndTime;
+    }
+
+    public void setToDateAndTime(String toDateAndTime) {
+        this.toDateAndTime = toDateAndTime;
+    }
+
+    public int getMaxKm() {
+        return maxKm;
+    }
+
+    public void setMaxKm(int maxKm) {
+        this.maxKm = maxKm;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
