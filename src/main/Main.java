94ca262d9311ff25bdb203a3892b94eeb967febe
moha <moha729g@stud.kea.dk>
2022-03-21 21:33:40 +0100

    Mo commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,14 @@ package main;
 import controller.ConsoleController;
 
 
-public class Main {
-
+public  class Main {
     public static void main(String[] args) {
         new ConsoleController().run();
     }
 }
+/*public class Main {
+
+    public static void main(String[] args) {
+        new ConsoleController().run();
+    }
+}*/
